9f6b1b21e0b29a3a463ecd2a5546e71113bccc86
slnpacifist <slnpacifist@gmail.com>
2020-07-15 01:03:37 +0300

    Added command support

--- app/main.rs
+++ app/main.rs
@@ -1,8 +1,9 @@
-use std::io::Error;
+use std::io::{Error, Write, stdout};
 use std::fs::File;
 use std::path::Path;
-use pitch_detection::{PitchDetector, AutocorrelationDetector, McLeodDetector};
+use pitch_detection::{PitchDetector, McLeodDetector};
 use wav::BitDepth;
+use clap::{Arg, App, SubCommand};
 
 const SAMPLE_RATE : usize = 500;
 const SIZE : usize = 512;
@@ -26,8 +27,9 @@ fn use_pitch_detector(data: &[f32]) -> Vec<f32> {
     res
 }
 
-fn main() -> Result<(), Error> {
-    let mut input_file = File::open(Path::new("data/2020-07-03/radio-transmission-recording.wav"))?;
+fn output_pitch_frequencies(input: &str) -> Result<(), Error> {
+    // "data/2020-07-03/radio-transmission-recording.wav"
+    let mut input_file = File::open(Path::new(input))?;
     let (_, raw_data) = wav::read(&mut input_file)?;
 
     let data: Vec<_> = match raw_data {
@@ -40,8 +42,32 @@ fn main() -> Result<(), Error> {
     };
 
     let data = use_pitch_detector(&data);
+    let stdout_instance = stdout();
+    let mut writer = stdout_instance.lock();
     for n in data.iter() {
-        println!("{}", n);
+        writer.write(n.to_string().as_bytes())?;
+        writer.write("\n".as_bytes())?;
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let args = App::new("My Super Program")
+        .about("Utilities for pegovka task: https://message-from-space.readthedocs.io/en/latest/radio-transmission-recording.html")
+        .subcommand(SubCommand::with_name("freqs")
+            .about("decodes input wave file into frequencies")
+            .arg(Arg::with_name("input")
+                .short("i")
+                .required(true)
+                .value_name("FILE")
+                .takes_value(true))
+                .help("input wave file")
+        )
+        .get_matches();
+
+    if let Some(freqs_args) = args.subcommand_matches("freqs") {
+        let input = freqs_args.value_of("input").unwrap();
+        output_pitch_frequencies(input)?;
     }
 
     Ok(())
