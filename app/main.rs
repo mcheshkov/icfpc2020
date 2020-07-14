30a1c019312a06c27bd28f43c073521fdad6dc1a
slnpacifist <slnpacifist@gmail.com>
2020-07-15 01:19:26 +0300

    Added window_size option

--- app/main.rs
+++ app/main.rs
@@ -1,21 +1,20 @@
 use std::io::{Error, Write, stdout};
 use std::fs::File;
 use std::path::Path;
+use std::str::FromStr;
 use pitch_detection::{PitchDetector, McLeodDetector};
 use wav::BitDepth;
 use clap::{Arg, App, SubCommand};
 
 const SAMPLE_RATE : usize = 500;
-const SIZE : usize = 512;
-const PADDING : usize = SIZE / 2;
 const POWER_THRESHOLD : f32 = 0.0;
 const CLARITY_THRESHOLD : f32 = 0.0;
 
-fn use_pitch_detector(data: &[f32]) -> Vec<f32> {
-    let mut detector = McLeodDetector::new(SIZE, PADDING);
+fn use_pitch_detector(data: &[f32], window_size: usize) -> Vec<f32> {
+    let mut detector = McLeodDetector::new(window_size, window_size / 2);
 
     let mut res = vec!();
-    for window in data.chunks(SIZE).filter(|x| x.len() == SIZE) {
+    for window in data.chunks(window_size).filter(|x| x.len() == window_size) {
         match detector.get_pitch(window, SAMPLE_RATE, POWER_THRESHOLD, CLARITY_THRESHOLD) {
             Some(pitch_res) => {
                 res.push(pitch_res.frequency);
@@ -27,7 +26,7 @@ fn use_pitch_detector(data: &[f32]) -> Vec<f32> {
     res
 }
 
-fn output_pitch_frequencies(input: &str) -> Result<(), Error> {
+fn output_pitch_frequencies(input: &str, window_size: usize) -> Result<(), Error> {
     // "data/2020-07-03/radio-transmission-recording.wav"
     let mut input_file = File::open(Path::new(input))?;
     let (_, raw_data) = wav::read(&mut input_file)?;
@@ -41,7 +40,7 @@ fn output_pitch_frequencies(input: &str) -> Result<(), Error> {
         }
     };
 
-    let data = use_pitch_detector(&data);
+    let data = use_pitch_detector(&data, window_size);
     let stdout_instance = stdout();
     let mut writer = stdout_instance.lock();
     for n in data.iter() {
@@ -60,14 +59,21 @@ fn main() -> Result<(), Error> {
                 .short("i")
                 .required(true)
                 .value_name("FILE")
-                .takes_value(true))
-                .help("input wave file")
+                .takes_value(true)
+                .help("input wave file"))
+            .arg(Arg::with_name("window-size")
+                .short("w")
+                .value_name("NUMBER")
+                .takes_value(true)
+                .default_value("1024")
+                .help("window size to calculate pitch frequency"))
         )
         .get_matches();
 
     if let Some(freqs_args) = args.subcommand_matches("freqs") {
         let input = freqs_args.value_of("input").unwrap();
-        output_pitch_frequencies(input)?;
+        let window_size = usize::from_str(freqs_args.value_of("window-size").unwrap()).unwrap();
+        output_pitch_frequencies(input, window_size)?;
     }
 
     Ok(())
