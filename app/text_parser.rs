1a24f0f890b6d0364ef409334f07d2a9ddf4b159
slnpacifist <slnpacifist@gmail.com>
2020-07-18 02:00:14 +0300

    Fixed action parser

--- app/text_parser.rs
+++ app/text_parser.rs
@@ -13,70 +13,116 @@ impl<'token> Token<'token> {
                     Token::parse_next(rest).expect("Could not parse first argument for ap");
                 let (second, rest) =
                     Token::parse_next(rest).expect("Could not parse second argument for ap");
-                (Action::Application(Box::new(first), Box::new(second)), rest)
+                (
+                    Action::SingleArgApplication(Box::new(first), Box::new(second)),
+                    rest,
+                )
             }
         })
     }
 }
 
-#[derive(Debug)]
-enum Action<'a> {
+#[derive(Debug, Eq, PartialEq)]
+pub enum Action<'a> {
     Value(&'a str),
-    Application(Box<Action<'a>>, Box<Action<'a>>),
+    SingleArgApplication(Box<Action<'a>>, Box<Action<'a>>),
+    MultipleArgApplication(Box<Action<'a>>, Vec<Action<'a>>),
+    List(Vec<Action<'a>>),
 }
 
 impl<'a> Action<'a> {
-    fn to_string(&self) -> String {
+    pub fn parse(stream: &str) -> Vec<Action> {
+        let test: Vec<Token> = stream
+            .split_ascii_whitespace()
+            .map(|x| match x {
+                "ap" => Token::Apply(),
+                _ => Token::Operand(x),
+            })
+            .collect();
+
+        let mut actions: Vec<Action> = vec![];
+        let mut stream = test.as_slice();
+        loop {
+            let next = Token::parse_next(stream);
+            match next {
+                Some((action, str)) => {
+                    stream = str;
+                    actions.push(action);
+                }
+                None => break,
+            }
+        }
+        actions
+    }
+
+    pub fn to_string(&self) -> String {
         match self {
             Self::Value(s) => String::from(*s),
-            Self::Application(func, operand) => {
+            Self::SingleArgApplication(func, operand) => {
                 format!("{}({})", func.to_string(), operand.to_string())
             }
+            Self::MultipleArgApplication(func, operands) => format!(
+                "{}({})",
+                func.to_string(),
+                operands
+                    .iter()
+                    .map(|o| o.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",")
+            ),
+            Self::List(items) => format!(
+                "[{}]",
+                items
+                    .iter()
+                    .map(|o| o.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",")
+            ),
         }
     }
 
-    fn to_math(&self) -> MathAction {
+    pub fn reduce_args(self) -> Action<'a> {
         match self {
-            Self::Value(s) => MathAction::Value(*s),
-            Self::Application(func, operand) => {
-                let mut tmp = func.to_math();
-                match tmp {
-                    MathAction::Value(_) => {
-                        MathAction::Call(Box::new(tmp), vec![operand.to_math()])
+            Self::SingleArgApplication(func, operand) => {
+                let mut first_reduced = func.reduce_args();
+                match first_reduced {
+                    Action::Value(_) | Action::List(_) => {
+                        Action::multi_application(first_reduced, vec![operand.reduce_args()])
                     }
-                    MathAction::Call(func, mut args) => {
-                        args.push(operand.to_math());
-                        MathAction::Call(func, args)
+                    Action::MultipleArgApplication(func, mut args) => {
+                        args.push(operand.reduce_args());
+                        Action::MultipleArgApplication(func, args)
                     }
-                    _ => unreachable!("should only be basic items"),
+                    Action::SingleArgApplication(_, _) => {
+                        unreachable!("cannot be SingleArgApplication")
+                    }
                 }
             }
+            Self::Value(_) => self,
+            Self::MultipleArgApplication(func, operands) => Self::multi_application(
+                func.reduce_args(),
+                operands.into_iter().map(|x| x.reduce_args()).collect(),
+            ),
+            Self::List(items) => Self::List(items.into_iter().map(|i| i.reduce_args()).collect()),
         }
     }
-}
 
-#[derive(Debug, Eq, PartialEq)]
-enum MathAction<'a> {
-    Value(&'a str),
-    Call(Box<MathAction<'a>>, Vec<MathAction<'a>>),
-    List(Vec<MathAction<'a>>),
-}
-
-impl<'a> MathAction<'a> {
-    fn combine_lists(self) -> MathAction<'a> {
+    fn reduce_lists(self) -> Action<'a> {
         match self {
-            Self::Call(val, mut args) if *val == MathAction::Value("cons") && args.len() == 2 => {
+            Self::MultipleArgApplication(val, mut args)
+                if *val == Self::Value("cons") && args.len() == 2 =>
+            {
                 let second = args.pop().unwrap();
                 let first = args.pop().unwrap();
-                let second_list_possibly = second.combine_lists();
+                let second_list_possibly = second.reduce_lists();
                 match second_list_possibly {
-                    MathAction::List(mut items) => {
+                    Self::List(mut items) => {
                         items.insert(0, first);
-                        MathAction::List(items)
+                        Self::List(items)
                     }
-                    MathAction::Value("nil") => MathAction::List(vec![first]),
-                    _ => MathAction::Call(
-                        Box::new(MathAction::Value("cons")),
+                    Self::Value("nil") => Self::List(vec![first]),
+                    _ => Self::MultipleArgApplication(
+                        Box::new(Self::Value("cons")),
                         vec![first, second_list_possibly],
                     ),
                 }
@@ -84,38 +130,58 @@ impl<'a> MathAction<'a> {
             _ => self,
         }
     }
+
+    fn application<'b>(func: Action<'b>, operand: Action<'b>) -> Action<'b> {
+        Action::SingleArgApplication(Box::new(func), Box::new(operand))
+    }
+
+    fn multi_application<'b>(func: Action<'b>, operands: Vec<Action<'b>>) -> Action<'b> {
+        Action::MultipleArgApplication(Box::new(func), operands)
+    }
 }
 
-// fn main() -> () {
-//     // let mut args: Vec<String> = env::args().collect();
-//     // let str = args.pop().expect("Need first argument");
-//
-//     // let tmp = "pwr2   =   ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1";
-//     let tmp = ":1029 = ap ap cons 7 ap ap cons 123229502148636 nil";
-//     let test: Vec<Token> = tmp.split_ascii_whitespace()
-//         .map(|x| {
-//             match x {
-//                 "ap" => Token::Apply(),
-//                 _ => Token::Operand(x),
-//             }
-//         })
-//         .collect();
-//     let mut actions: Vec<Action> = vec![];
-//     let mut stream = test.as_slice();
-//     loop {
-//         let next = Token::parse_next(stream);
-//         match next {
-//             Some((action, str)) => {
-//                 stream = str;
-//                 actions.push(action);
-//             }
-//             None => break
-//         }
-//     }
-//
-//     for a in actions {
-//         let m = a.to_math();
-//         println!("{:?}", m);
-//         println!("{:?}", m.combine_lists());
-//     }
-// }
+#[test]
+fn test_parser() {
+    let test1 = ":1029 = ap ap cons 7 ap ap cons 123229502148636 nil";
+    let exp1 = vec![
+        Action::Value(":1029"),
+        Action::Value("="),
+        Action::application(
+            Action::application(Action::Value("cons"), Action::Value("7")),
+            Action::application(
+                Action::application(Action::Value("cons"), Action::Value("123229502148636")),
+                Action::Value("nil"),
+            ),
+        ),
+    ];
+    assert_eq!(Action::parse(test1), exp1);
+}
+
+#[test]
+fn test_reduce_args() {
+    let test1 = "ap ap cons 7 ap ap cons 123229502148636 nil";
+    let token = Action::parse(test1).pop().unwrap();
+    let exp1 = Action::multi_application(
+        Action::Value("cons"),
+        vec![
+            Action::Value("7"),
+            Action::multi_application(
+                Action::Value("cons"),
+                vec![Action::Value("123229502148636"), Action::Value("nil")],
+            ),
+        ],
+    );
+    let res = token.reduce_args();
+    assert_eq!(res, exp1);
+}
+
+#[test]
+fn test_reduce_lists() {
+    let test1 = "ap ap cons 7 ap ap cons 123229502148636 nil";
+    let token = Action::parse(test1).pop().unwrap();
+    let multi_token = token.reduce_args();
+    let exp1 = Action::List(vec![Action::Value("7"), Action::Value("123229502148636")]);
+
+    let res = multi_token.reduce_lists();
+    assert_eq!(res, exp1);
+}
