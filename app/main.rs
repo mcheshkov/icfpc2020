db8dc1a07a1c65e706f68af0cf95199abb2e8ca4
aanper <mail@s3f.ru>
2020-07-17 21:03:04 +0300

    send zero-request

--- app/main.rs
+++ app/main.rs
@@ -1,22 +1,18 @@
 use http_body::Body as _;
 use hyper::{Client, Request, Method, Body, StatusCode};
+use hyper::client::HttpConnector;
 use std::env;
 use std::process;
+use std::thread;
+use std::time;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let args: Vec<String> = env::args().collect();
+async fn send_to_aliens(client: &Client<HttpConnector>, url: &str, body: String) ->
+    Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-    let server_url = &args[1];
-    let player_key = &args[2];
-
-    println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
-
-    let client = Client::new();
     let req = Request::builder()
         .method(Method::POST)
-        .uri(server_url)
-        .body(Body::from(format!("{}", player_key)))?;
+        .uri(url.to_owned() + "/aliens/send")
+        .body(Body::from(body))?;
 
     match client.request(req).await {
         Ok(mut res) => {
@@ -49,6 +45,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             process::exit(1);
         }
     }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let args: Vec<String> = env::args().collect();
+
+    let server_url = &args[1];
+    let player_key = &args[2];
+
+    println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
+
+    let client = Client::new();
+    
+    send_to_aliens(&client, &server_url, "1101000".to_owned()).await?;
+    thread::sleep(time::Duration::from_secs(12));
+    send_to_aliens(&client, &server_url, "1101000".to_owned()).await?;
+    thread::sleep(time::Duration::from_secs(6));
+    send_to_aliens(&client, &server_url, "1101000".to_owned()).await?;
 
     Ok(())
 }
