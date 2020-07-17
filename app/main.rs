bf46ea9c1b28f3ea386cca849f8219314695e88d
aanper <mail@s3f.ru>
2020-07-17 23:25:26 +0300

    use playerkey and url from cli

--- app/main.rs
+++ app/main.rs
@@ -13,10 +13,11 @@ async fn send_to_aliens(
     client: &Client<HttpsConnector<HttpConnector>>,
     url: &str,
     body: String,
+    player_key: &str
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let req = Request::builder()
         .method(Method::POST)
-        .uri(url.to_owned() + "/aliens/send?apiKey=4b5b59dead9e42fbbf203df4e634a2da")
+        .uri(url.to_owned() + "/aliens/send?apiKey=" + &player_key.to_owned())
         .body(Body::from(body))?;
 
     match client.request(req).await {
@@ -56,20 +57,20 @@ async fn send_to_aliens(
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let args: Vec<String> = env::args().collect();
 
-    // let server_url = &args[1];
-    const server_url: &str = "https://icfpc2020-api.testkontur.ru";
+    let server_url = &args[1];
+    // const server_url: &str = "https://icfpc2020-api.testkontur.ru";
     let player_key = &args[2];
 
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
-    
+
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, Body>(https);
     
-    send_to_aliens(&client, &server_url, "1101000\n".to_owned()).await?;
+    send_to_aliens(&client, &server_url, "1101000\n".to_owned(), &player_key).await?;
     thread::sleep(time::Duration::from_secs(12));
-    send_to_aliens(&client, &server_url, "1101000\n".to_owned()).await?;
+    send_to_aliens(&client, &server_url, "1101000\n".to_owned(), &player_key).await?;
     thread::sleep(time::Duration::from_secs(6));
-    send_to_aliens(&client, &server_url, "1101000\n".to_owned()).await?;
+    send_to_aliens(&client, &server_url, "1101000\n".to_owned(), &player_key).await?;
 
     Ok(())
 }
