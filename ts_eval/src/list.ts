a2efc757c2127e0d443a369caa87d0c0ee456af7
Mikhail Cheshkov <mcheshkov@gmail.com>
2020-07-18 20:29:08 +0300

    Fix ListCons for empty lists again

--- ts_eval/src/list.ts
+++ ts_eval/src/list.ts
@@ -1,14 +1,22 @@
 import {Lam, LamList} from "./common";
-import {t} from "./symbols";
+import {nil, t} from "./symbols";
 
-export function ListCons(items: Array<Lam>): Lam & LamList {
+export function ListCons(items: Array<Lam>): Lam {
+    if (items.length === 0) return nil;
+
     const res: Lam & LamList = function list(x2: Lam): Lam {
         if (items.length === 0) {
             return t;
         }
 
         let x0 = items[0];
-        let x1 = ListCons(items.slice(1));
+        const tail = items.slice(1);
+        let x1;
+        if (tail.length > 0) {
+            x1 = ListCons(tail);
+        } else {
+            x1 = nil;
+        }
         return x2(x0)(x1);
     } as any;
     res.type = "list";
