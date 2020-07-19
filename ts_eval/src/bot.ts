d2f254de816d2c67157ed9cce3e2e95f29a05ee9
Mikhail Cheshkov <mcheshkov@gmail.com>
2020-07-20 01:19:38 +0300

    Add simple gravity simulation, use stand still as base strat

--- ts_eval/src/bot.ts
+++ ts_eval/src/bot.ts
@@ -31,6 +31,56 @@ function sign(a:bigint): bigint {
     return a < 0n ? -1n : a > 0n ? 1n : 0n;
 }
 
+function grav(pos: VecS): VecS {
+    if (abs(pos[0]) > abs(pos[1])) {
+        // gravity now works on X
+        // will accel ship by [+-1, 0]
+        return [
+            -1n*sign(pos[0]),
+            0n,
+        ];
+    } else if (abs(pos[0]) < abs(pos[1])) {
+        // gravity now works on Y
+        // will accel ship by [0, +-1]
+        return [
+            0n,
+            -1n*sign(pos[1]),
+        ];
+    } else {
+        // gravity now works on both
+        // will accel ship by [+-1, +-1]
+        return [
+            -1n*sign(pos[0]),
+            -1n*sign(pos[1]),
+        ];
+    }
+}
+
+function simulateGrav(startPos: VecS, startVel: VecS, accs: Array<VecS>, steps:number, cb: (pos: VecS) => void) {
+    let pos = startPos;
+    let vel = startVel;
+
+    for (let i=0; i<steps; i++) {
+        let totalAcc: VecS = [0n,0n];
+        totalAcc = add(totalAcc, grav(pos));
+        if (accs.length > 0) {
+            let [acc, ...tail] = accs;
+            totalAcc = add(totalAcc, acc);
+            accs = tail;
+        }
+        vel = add(vel, totalAcc);
+        pos = add(pos, vel);
+
+        cb(pos);
+    }
+}
+
+function willHit(startPos: VecS, startVel: VecS, steps:number): boolean {
+    let result = false;
+    simulateGrav(startPos, startVel, [], steps, pos => result = result || (norm(pos) < 16));
+    return result;
+}
+
 export class Bot {
     protected client: Client;
 
@@ -69,27 +119,45 @@ export class Bot {
 
                 let thrust = tangent;
 
-                if (state.tick < 8 ) {
-                    if (abs(pos[0]) > abs(pos[1])) {
-                        // gravity now works on X
-                        thrust = [
-                            0n,
-                            -1n*sign(pos[1])
-                        ]
-                    } else if (abs(pos[0]) > abs(pos[1])) {
-                        // gravity now works on Y
-                        thrust = [
-                            -1n*sign(pos[0]),
-                            0n,
-                        ]
-                    } else {
-                        // gravity now works on both
-                        thrust = [
-                            -1n*sign(pos[0]),
-                            -1n*sign(pos[1]),
-                        ]
+                function orbit() {
+                    if (state.tick < 8) {
+                        if (abs(pos[0]) > abs(pos[1])) {
+                            // gravity now works on X
+                            // will accel ship by [+-1, 0]
+                            thrust = [
+                                -1n*sign(pos[0]), // fight gravity
+                                sign(pos[1]), // slide on loger arc
+                            ];
+                        } else if (abs(pos[0]) < abs(pos[1])) {
+                            // gravity now works on Y
+                            // will accel ship by [0, +-1]
+                            thrust = [
+                                sign(pos[0]), // slide on loger arc
+                                -1n*sign(pos[1]), // fight gravity
+                            ];
+                        } else {
+                            // gravity now works on both
+                            // will accel ship by [+-1, +-1]
+                            thrust = [
+                                -1n*sign(pos[0]), // fight gravity
+                                -1n*sign(pos[1]), // fight gravity
+                            ]
+                        }
+                    } else if (willHit(ship.position, ship.velocity, 4)) {
+                        // Это не работает - ускорители не могут преодолеть гравитацию, и скорость не уменьшается, надо искать другой манёвр
+                        const g = grav(ship.position);
+                        thrust = g; // point thruster to gravity direction
                     }
                 }
+
+                function standStill() {
+                    // Получается очень дорого - чем дальше тем больше "топлива" тратится на каждое ускорение
+                    const g = grav(ship.position);
+                    thrust = g; // point thruster to gravity direction
+                }
+
+                //orbit();
+                standStill();
 
                 // if (vel > 7) {
                 //     thrust = [0n, 1n];
