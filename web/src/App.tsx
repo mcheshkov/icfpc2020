df40ac6c3c3525eb6b2cf34777afa8f6db7e725f
aanper <mail@s3f.ru>
2020-07-19 13:31:11 +0300

    handle click position

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,13 +9,23 @@ import {main as fn_main} from 'fn_js/build/main';
 function App() {
     const canvas = useRef<HTMLCanvasElement>(null);
 
-    const handleClick = () => {
+    const PIXEL_SIZE = 5;
+    const WIDTH = 300;
+    const HEIGHT = 300;
+
+    const handleClick = (e: any) => {
+        console.log(e);
         if(canvas !== null) {
             let current = canvas.current;
             if(current !== null) {
                 let context = current.getContext('2d');
                 console.log(canvas.current);
                 if(context !== null) {
+                    let x = (e.pageX - current.offsetLeft - WIDTH/2) / PIXEL_SIZE | 0;
+                    let y = -(e.pageY - current.offsetTop - HEIGHT/2)  / PIXEL_SIZE | 0;
+
+                    console.log(x, y);
+
                     fn_main(context);
                 }
             }
